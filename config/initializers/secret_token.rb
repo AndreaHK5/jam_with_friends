881cdc7552e8015df4876a420512b86e21f090d3
Andrea <cremese@gmail.com>
2013-12-14 09:43:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JamWithFriends::Application.config.secret_key_base = '75e1fb1621edc7086cc6df1483367e08b5f053ceb1e0d4561b27b62861260b4b7b0bdb55c38ce21c5000090ab3d5175d4239efe60c05a2b190d84af901fcca87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JamWithFriends::Application.config.session_store :cookie_store, key: '_jam_with_friends_session'
