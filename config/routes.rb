c0cb143bec6b79bad76afdd2bbf9f03a6244c91d
Andrea <cremese@gmail.com>
2013-12-16 18:03:51 -0500

    registration does not work, will need to override the controllers of devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ JamWithFriends::Application.routes.draw do
   get "search/index", to: 'search#index', as: "search"
   resources :users, only: [:edit, :update]
   get "users/show_profile", to: 'users#show_profile', as: "show_profile"
-  get "users/edit_profile", to: 'users#edit_profile', as: "edit_profile"      
-  get "users/show_map", to: 'users#show_map', as: "show_map"
+  get "users/edit_profile", to: 'users#edit_profile', as: "edit_profile"
   get "users/show_other_profile/:id", to: 'users#show_other_profile', as:"show_other_profile"
   resources :instruments
   resources :generes
