d1c1e8fc6216d52039d4da286397394d3373c297
Andrea <cremese@gmail.com>
2013-12-30 11:57:46 -0500

    beter style for the top bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,24 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      if current_user.location == nil
-        @location_search = current_location
-      else
-        @location_search = current_user.location.address
-      end
-    else
-      @location_search = current_location
-    end
-    @locations = Location.near(@location_search,2)
-    @users = @locations.each.collect {|l| l.user}
-    @users.delete(current_user)
-    prepare_hash_for_map
-    @instruments = Instrument.all
-    @generes = Genere.all
+    # if user_signed_in?
+    #   if current_user.location == nil
+    #     @location_search = current_location
+    #   else
+    #     @location_search = current_user.location.address
+    #   end
+    # else
+    #   @location_search = current_location
+    # end
+    # @locations = Location.near(@location_search,2)
+    # @users = @locations.each.collect {|l| l.user}
+    # @users.delete(current_user)
+    # prepare_hash_for_map
+    # @instruments = Instrument.all
+    # @generes = Genere.all
+
+@users =[User.first]
+@instruments = [Instrument.first]
+@generes = [Genere.first]
+
   end
 end
