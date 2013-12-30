7fb4eff7bd750572d223de2c7f901cc797f24b4a
Andrea <cremese@gmail.com>
2013-12-30 15:57:55 -0500

    header now working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,19 @@
 class HomeController < ApplicationController
   def index
-    # if user_signed_in?
-    #   if current_user.location == nil
-    #     @location_search = current_location
-    #   else
-    #     @location_search = current_user.location.address
-    #   end
-    # else
-    #   @location_search = current_location
-    # end
-    # @locations = Location.near(@location_search,2)
-    # @users = @locations.each.collect {|l| l.user}
-    # @users.delete(current_user)
-    # prepare_hash_for_map
-    # @instruments = Instrument.all
-    # @generes = Genere.all
-
-@users =[User.first]
-@instruments = [Instrument.first]
-@generes = [Genere.first]
-
+    if user_signed_in?
+      if current_user.location == nil
+        @location_search = current_location
+      else
+        @location_search = current_user.location.address
+      end
+    else
+      @location_search = current_location
+    end
+    @locations = Location.near(@location_search)
+    @users = @locations.each.collect {|l| l.user}
+    @users.delete(current_user)
+    prepare_hash_for_map
+    @instruments = Instrument.all
+    @generes = Genere.all
   end
 end
