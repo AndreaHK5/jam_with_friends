f0d55ae743e3ae635cf9fddaa23e6e68129d7045
Andrea <cremese@gmail.com>
2014-01-11 12:22:00 -0500

    some styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,10 +16,18 @@ class HomeController < ApplicationController
     prepare_hash_for_map
     @instruments_current = []
     @genres_current = []
+    if user_signed_in?
+      if current_user.instrxps.empty? || current_user.location.nil? || current_user.genres.empty?
+        flash[:notice] = "update your profile to become more searcheable, missing"
+        current_user.instrxps.empty? ? flash[:notice] << " instruments" : ""
+        current_user.genres.empty? ? flash[:notice] << " genres" : ""
+        current_user.location.nil? ? flash[:notice] << " location" : ""
+      end
+    end
+
     # @users =[User.first]
     # @instruments = [Instrument.first]
     # @genres = [Genre.first]
     # @location_search = "20 crooke ave 11226 ny"
-
   end
 end
