bf878bdcc7816c4c8ece39cd938903bdc60fbed6
Andrea <cremese@gmail.com>
2014-02-22 17:22:33 -0500

    candidates working

--- app/controllers/jam_managements_controller.rb
+++ app/controllers/jam_managements_controller.rb
@@ -19,10 +19,30 @@ class JamManagementsController < ApplicationController
     end
   end
 
+  def edit_candidates
+    @jam = Jam.find(params[:jam_id].to_i)
+    @instrument = Instrument.search_by_name(params[:instrument_name]).first
+    @candidates = Candidate.where(jam_id: @jam.id, instrument_id: @instrument.id)
+  end
+
+  def update_candidates
+    @jam = Jam.find(params[:jam_id].to_i)
+    @instrument = Instrument.find(params[:instrument_id].to_i)
+    @prev_candidates = Candidate.where(jam_id: @jam.id, instrument_id: @instrument.id)
+    @prev_candidates.each {|c| c.delete}
+    # delete the candidates for 
+    params[:users][:id].each do |id|
+      unless id == ""
+        Candidate.create(user_id: id.to_i, jam_id: @jam.id, instrument_id: @instrument.id)
+      end
+    end
+    redirect_to :action => :show, :jam_id => @jam.id
+  end
+
   private
   
   def find_jam
-    sea = params[:id].to_i
+    sea = params[:jam_id].to_i
     @jam = Jam.find sea
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ JamWithFriends::Application.routes.draw do
   get "registrations/after_sign_up_path_for"
   root "home#index"
   get "search/index", to: 'search#index', as: "search"
+  get "jam_managements/edit_candidates", to: 'jam_managements#edit_candidates', as: "edit_candidates"
+  get "jam_managements/update_candidates", to: 'jam_managements#update_candidates', as: "update_candidates"
   resources :instruments
   resources :genres
   devise_for :users, :controllers => { :registrations => "registrations", :passwords => "passwords"  }
